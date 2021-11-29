a1a78e7a2e8740f2be259f7853228c7c3caff698
harishreyya <harishrebel0@gmail.com>
2021-11-29 16:03:54 +0530

    schema updated

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const express = require("express")
+const express = require("express");
 
-const jobs = require("./jobs")
+const mongoose = require("mongoose");
+const { stringify } = require("querystring");
 
+const jobs = require("./jobs.json")
+
+const connect = () => {
+    return mongoose.connect(" mongodb://127.0.0.1:27017/test")
+}
+const jobSchema = new mongoose.Schema({
+    company_name : {type: String, required: true},
+    jobs:{type: String, required:true},
+    skill_set:{type:String, required:true},
+    notice_period:{type:Number,required:true}
+});
 const app = express()
 
 app.use(express.json());
@@ -10,6 +22,7 @@ app.get("/",(req,res)=>{
     res.send(jobs)
 })
 
-app.listen(2345,function(){
+app.listen(2345, async function(){
+    await connect();
     console.log("listening on port 2345")
 })
